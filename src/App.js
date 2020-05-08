afc5f26b88ad340a72b3ccee45049e35d1001056
alazo <alazo@pop-os.localdomain>
2020-05-07 19:55:35 -0700

    Initialzed react project and installed initial packages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const App = () => {
+    return <h1>Hello World</h1>;
+};
